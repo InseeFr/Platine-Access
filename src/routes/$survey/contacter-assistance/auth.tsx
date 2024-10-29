3c6113a491546e513993205fd1315e098cfeb9d0
Lea Renaux <lea.renaux@insee.fr>
2024-10-29 11:06:54 +0100

    feat: remove useEffect in login page

--- src/routes/$survey/contacter-assistance/auth.tsx
+++ src/routes/$survey/contacter-assistance/auth.tsx
@@ -21,7 +21,7 @@ function SupportPage() {
   const { t } = useTranslation("Support");
   const search = useSearch({ from: "/$survey/contacter-assistance/auth" });
   const { survey } = Route.useParams();
-  const isAuthenticated = useIsAuthenticated();
+  const { isAuthenticated } = useIsAuthenticated();
 
   if (!search.questioningId || !isAuthenticated) {
     return <Navigate to={"/$survey/contacter-assistance"} params={{ survey: survey }} />;
