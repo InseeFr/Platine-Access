c3d80fff2b3cdda58f1c7068880bfe2e8e70e89c
Lea Renaux <lea.renaux@insee.fr>
2025-01-14 15:44:05 +0100

    feat: add chatbot missing attribute

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -7,6 +7,7 @@ export const Chatbot = () => {
         agent-id="4c3b1ceb-466c-444d-ac67-c24f44da2f98"
         language-code="fr"
         max-query-length="-1"
+        intent="default"
       >
         <df-messenger-chat-bubble
           chat-title-icon="https://entreprises.stat-publique.fr/img/favicon.ico"
